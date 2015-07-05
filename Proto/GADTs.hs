4857cc702cf25150e7ebc4dcdb83ce97366e485d
Kaivan Shah <kaivan29@tamu.edu>
2015-07-05 17:31:53 -0500

    Create GADTs.hs

---
+++ Proto/GADTs.hs
@@ -0,0 +1,47 @@
+-- The following language extensions need to be enabled:
+-- DeriveDataTypeable, FlexibleInstances, MultiParamTypeClasses
+
+{-# LANGUAGE FlexibleContexts, DeriveDataTypeable, FlexibleInstances, MultiParamTypeClasses, StandaloneDeriving, ExistentialQuantification , GADTs #-}
+
+import GenProg
+import GenProg.GenExpr
+import GenProg.GenExpr.Data
+import Data.Generics
+import Control.Monad
+import Control.Monad.Random
+import Data.Fixed
+
+class IsGenExp t where
+      mutate :: (MonadRandom m) => t -> m t
+      crossOver :: (MonadRandom m) => t -> t -> m t
+      randomGen :: (MonadRandom m) => m t
+	  
+data GenExp = forall t . IsGenExp t => GenExp t
+
+data Expr a where
+    I :: Int -> Expr Int    --Our integer constants and 'a' is 
+    B :: Bool -> Expr Bool   --Boolean constants
+    Plus :: Expr Int -> Expr Int -> Expr Int
+    Times :: Expr Int -> Expr Int -> Expr Int
+    Eq :: Expr Int -> Expr Int -> Expr Bool
+
+deriving instance Typeable (Expr a)
+--Expr = forall a . Data a => Expr a
+--deriving instance Data a => Data (Expr a)
+deriving instance Eq (Expr a)
+deriving instance Show (Expr a)
+	   
+eval :: Expr a -> a --Maybe (Either Int Bool)
+eval (I n) = n
+eval (B b) = b
+eval (Plus e1 e2) = eval e1 + eval e2
+eval (Times e1 e2) = eval e1 * eval e2
+eval (Eq e1 e2) = eval e1 == eval e2
+
+instance GenProg (Rand StdGen) (Expr a) where
+  terminal    = I `liftM` getRandomR (1,9)
+  nonterminal = do
+    r <- getRandomR (0,2)
+    [liftM2 Plus terminal terminal,
+     liftM2 Times terminal terminal,
+	 liftM2 Eq terminal terminal] !! r
