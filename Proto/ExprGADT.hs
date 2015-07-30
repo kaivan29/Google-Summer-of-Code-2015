1081abf8112fc77ff037122ff0c08151bfb27bc8
Kaivan Shah <kaivan29@tamu.edu>
2015-07-30 00:45:24 -0500

    Update ExprGADT.hs

--- Proto/ExprGADT.hs
+++ Proto/ExprGADT.hs
@@ -40,3 +40,56 @@ data Op2 :: * -> * -> * where
     LEquals :: Op2 Int Bool
     And    :: Op2 Bool Bool
     Or    :: Op2 Bool Bool
+
+deriving instance Show (Indexor ks k)
+-- deriving instance Show a => Show (Expr vs a)
+
+main :: IO ()
+main = print . eval $ Foldr (V IZ + V (IS IZ)) (I 0) (List [I 1, I 2, I 3])
+
+eval :: Expr '[] a -> a
+eval (I i)       = i
+eval (B b)       = b
+eval (V v)       = error $ "Unexpected unbound variable " ++ show v ++ "...should be impossible really."
+eval (O1 o e1) = op1 o (eval e1)
+eval (O2 o e1 e2) = op2 o (eval e1) (eval e2)
+eval (e1 :$ e2)  = eval $ reduceWith e2 e1
+eval (List es)   = map eval es
+eval (Foldr ef ez el) = case el of
+                          List es -> eval $ foldr (\x z -> (ef :$ Const x) :$ z) ez es
+                          _       -> error "Unexpected list type not formed by List constructor"
+
+
+op1 :: Op1 a -> a -> a
+op1 Abs = abs
+op1 Signum = signum
+op1 Not = not
+
+op2 :: Op2 a b -> a -> a -> b
+op2 Plus = (+)
+op2 Times = (*)
+op2 Minus = (-)
+op2 LEquals = (<=)
+op2 And = (&&)
+op2 Or = (||)
+
+reduceWith :: Expr vs v -> Expr (v ': vs) a -> Expr vs a
+reduceWith ev el = case el of
+                     I i        -> I i
+                     B b        -> B b
+                     V IZ       -> ev
+                     V (IS v)   -> V v
+                     Const e1   -> e1
+                     O1 o e1    -> O1 o $ reduceWith ev e1
+                     O2 o e1 e2 -> O2 o (reduceWith ev e1) (reduceWith ev e2)
+                     e1 :$ e2   -> reduceWith ev (reduceWith e2 e1)
+                     List es    -> List $ map (reduceWith ev) es
+                     -- Foldr ef ez el -> Foldr (reduceWith ev ef) (reduceWith ev ez) (reduceWith ez el)
+
+instance Num (Expr vs Int) where
+    (+) = O2 Plus
+    (*) = O2 Times
+    (-) = O2 Minus
+    abs = O1 Abs
+    signum = O1 Abs
+    fromInteger = I . fromInteger
